29af7c7c5ae25503500502d2384d7191e1363e7e
Bilal Ahmed <bilalahmedim@gmail.com>
2021-06-06 12:39:47 +0530

    in progress..

---
+++ pattern.py
@@ -0,0 +1,20 @@
+import time, sys
+indent = 0
+indentIncreasing = True
+
+try:
+  while True:
+    print(' ' * indent, end='')
+    print('********')
+    time.sleep(0.1) # Pause for 1/10 of a second.
+
+    if indentIncreasing:
+      indent = indent + 1
+      if indent == 20:
+        indentIncreasing = False
+    else:
+      indent = indent - 1
+      if indent == 0:
+        indentIncreasing = True
+except KeyboardInterrupt:
+  sys.exit()
